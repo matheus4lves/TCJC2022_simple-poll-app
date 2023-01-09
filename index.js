060c75c57032f12d2aee9d66efe5330655bee40f
Matheus Alves <matheusalves.bmd@gmail.com>
2023-01-09 16:12:32 -0300

    Question the user

--- index.js
+++ index.js
@@ -3,4 +3,16 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = window.prompt(`
+    What is your favorite programming language?
+    0: JavaScript
+    1: Python
+    2: Rust
+    3: C++
+    (Write option number)
+    `);
+  },
 };
+
+poll.registerNewAnswer();
